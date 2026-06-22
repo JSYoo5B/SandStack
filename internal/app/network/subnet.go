3b2b594daac77bd49ceb683a8e9065785c02c83f
JaeSang Yoo <jsyoo5b@gmail.com>
2026-06-23 01:33:04 +0900

    refactor(network): split app service by resource

---
+++ internal/app/network/subnet.go
@@ -0,0 +1,104 @@
+package network
+
+import (
+	"errors"
+
+	"github.com/JSYoo5B/SandStack/internal/platform/idgen"
+)
+
+var ErrSubnetNotFound = errors.New("subnet not found")
+
+func (s *Service) ListSubnets() []Subnet {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	subnets := make([]Subnet, 0, len(s.subnetIDs))
+	for _, id := range s.subnetIDs {
+		subnets = append(subnets, s.subnets[id])
+	}
+
+	return subnets
+}
+
+func (s *Service) CreateSubnet(input CreateSubnet) Subnet {
+	enableDHCP := true
+	if input.EnableDHCP != nil {
+		enableDHCP = *input.EnableDHCP
+	}
+
+	subnet := Subnet{
+		ID:             "subnet-" + idgen.RandomHex(16),
+		NetworkID:      input.NetworkID,
+		Name:           input.Name,
+		Description:    input.Description,
+		IPVersion:      input.IPVersion,
+		CIDR:           input.CIDR,
+		GatewayIP:      input.GatewayIP,
+		DNSNameservers: input.DNSNameservers,
+		EnableDHCP:     enableDHCP,
+		TenantID:       input.ProjectID,
+		ProjectID:      input.ProjectID,
+	}
+
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.subnetIDs = append(s.subnetIDs, subnet.ID)
+	s.subnets[subnet.ID] = subnet
+
+	network, ok := s.networks[subnet.NetworkID]
+	if ok {
+		network.Subnets = append(network.Subnets, subnet.ID)
+		s.networks[subnet.NetworkID] = network
+	}
+
+	return subnet
+}
+
+func (s *Service) GetSubnet(id string) (Subnet, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	subnet, ok := s.subnets[id]
+	if !ok {
+		return Subnet{}, ErrSubnetNotFound
+	}
+
+	return subnet, nil
+}
+
+func (s *Service) DeleteSubnet(id string) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	subnet, ok := s.subnets[id]
+	if !ok {
+		return ErrSubnetNotFound
+	}
+
+	delete(s.subnets, id)
+	for index, currentID := range s.subnetIDs {
+		if currentID == id {
+			s.subnetIDs = append(s.subnetIDs[:index], s.subnetIDs[index+1:]...)
+			break
+		}
+	}
+
+	network, ok := s.networks[subnet.NetworkID]
+	if ok {
+		network.Subnets = removeString(network.Subnets, id)
+		s.networks[subnet.NetworkID] = network
+	}
+
+	return nil
+}
+
+func removeString(values []string, target string) []string {
+	for index, value := range values {
+		if value == target {
+			return append(values[:index], values[index+1:]...)
+		}
+	}
+
+	return values
+}
